964c46e0d7ea63f3f0bf861274fd4295dafcd38a
mrashid124 <mrashidraju@gmail.com>
2025-04-09 22:03:19 +0100

    All food api apdated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,13 +35,131 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+
+    const foodsCollection = client.db("tadooriFreshDB").collection("allFoods");
+
+    const galleryCollection = client
+      .db("tadooriFreshDB")
+      .collection("allGallery");
+    
+    const purchaseFoodsCollection = client
+      .db("tadooriFreshDB")
+      .collection("purchaseFoods");
+    
+    const usersCollection = client.db("tadooriFreshDB").collection("allUsers");
+
+    // Generate jwt
+
+
+    // Create Token
+
+
+    // Get all Foods data from DB api
+    app.get("/allfoods", async (req, res) => {
+        const cursor = foodsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+
+
+   // Save all Foods data in DB api
+      app.post("/allFoods", async (req, res) => {
+        const newFood = req.body;
+        const result = await foodsCollection.insertOne(newFood);
+        res.send(result);
+      });
+
+    //   Single food by ID
+      app.get("/singleFood/:id", async (req, res) => {
+        const id = req.params.id;
+        const result = await foodsCollection.findOne({ _id: new ObjectId(id) });
+        res.send(result);
+      });
+
+          // Gallery Collection api
+    app.get("/gallery", async (req, res) => {
+        const cursor = galleryCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+  
+      app.post("/gallery", async (req, res) => {
+        const newGallery = req.body;
+        const result = await galleryCollection.insertOne(newGallery);
+        res.send(result);
+      });
+
+
+
+
+      app.post("/users", async (req, res) => {
+        const newUser = req.body;
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      });
+
+
+
+
+
+
+          // for pagination
+
+    app.get("/allFoodsForPagination", async (req, res) => {
+      const size = parseInt(req.query.size);
+      const page = parseInt(req.query.page) - 1;
+      const filter = req.query.filter;
+      const sort = req.query.sort;
+      const search = req.query.search;
+
+      let query = {
+        food_name: { $regex: search, $options: "i" },
+      };
+
+      if (filter) {
+        query.food_category = filter;
+      }
+
+      let processes = [
+        { $match: query },
+        { $addFields: { numberPrice: { $toDouble: "$price" } } },
+        { $sort: { numberPrice: sort === "low" ? 1 : -1 } },
+        { $project: { numberPrice: 0 } },
+        { $skip: size * page },
+        { $limit: size },
+      ];
+
+      const result = await foodsCollection.aggregate(processes).toArray();
+      res.send(result);
+    });
+
+
+    // data count
+    app.get("/allFoodsCont", async (req, res) => {
+      const filter = req.query.filter;
+      const search = req.query.search;
+
+      let query = {
+        food_name: { $regex: search, $options: "i" },
+      };
+      if (filter) {
+        query.food_category = filter;
+      }
+      const count = await foodsCollection.countDocuments(query);
+      res.send({ count });
+    });
+
+
+    
+
+
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -55,3 +173,7 @@ res.send('Tandoori server is running')
 app.listen(port, () => {
     console.log(`Tandoori server is running on port: ${port}`)
 })
+
+
+
+
